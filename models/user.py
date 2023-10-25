43431c04846f6706d21625df06feda423f4f9f59
SikhoSwart <sikhoswart@gmail.com>
2023-10-25 12:01:13 -0700

    classes

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+
+# SQLAlchemy modules
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+    Defines a class User
+    Attributes:
+        __tablename__ (str): Users MySQL table name
+    """
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
