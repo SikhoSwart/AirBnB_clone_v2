0d324591b0ae6973bb4bea332aebe44deda312aa
SikhoSwart <sikhoswart@gmail.com>
2023-10-25 12:19:13 -0700

    More Classes!

--- models/user.py
+++ models/user.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
 from models.base_model import BaseModel, Base
-
-# SQLAlchemy modules
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from models.review import Review
 
 
 class User(BaseModel, Base):
+    """This is the class for user
     """
-    Defines a class User
-    Attributes:
-        __tablename__ (str): Users MySQL table name
-    """
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade="all, delete", backref="user")
+    reviews = relationship("Review", cascade="all, delete", backref="user")
